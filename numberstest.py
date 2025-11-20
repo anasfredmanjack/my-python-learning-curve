f2cdbcda5e89ca554eec8c3a9e2e5fb652edbadc
anas Fred <anasfred3@gmail.com>
2025-11-20 13:29:53 -0800

    python arithmetric operations

---
+++ numberstest.py
@@ -0,0 +1,27 @@
+### how to check the type of character it or a variable is
+print(type(5))
+
+### addition
+print(type(5+5))
+
+## multiplication
+print(type(5*5))
+
+##subtraction
+print(type(5-5))
+
+### division note: when dividing integar it returns float thats how python3 works
+print(type(5/5))
+
+### division note: how to return integar when dividing an integar
+print(type(5//5))
+
+### how to get the powrup of a number in python 
+print(type(5**5))
+
+### how to get the remainder of a number 
+print(10%3)
+
+### python follows (BIDMAS) (Bracket, Indices, Division, Multiplication, Addition and Subtraction) 
+print(5+5*3)
+
