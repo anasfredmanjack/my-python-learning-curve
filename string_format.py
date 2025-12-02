deec41fde00dd74beeb757ef4d3fc463dd075927
anas Fred <anasfred1677@gmail.com>
2025-12-01 16:32:46 -0800

    string format

---
+++ string_format.py
@@ -0,0 +1,12 @@
+num1 = 3.142567898
+num2 = 10.23455353
+
+#normal method 
+print('num 1 is: ', num1, 'and num2 is: ', num2)
+
+#usig the format method
+print('num 1 is: {0:.3f} and num2 is: {1:.4f}'.format(num1, num2))
+
+
+#usig the f strings methid
+print(f'num 1 is: {num1:.3f} and num2 is: {num2:.4f}')
