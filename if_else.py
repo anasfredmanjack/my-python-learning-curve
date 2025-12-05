2a6aeff4c42e860fb729643e61174428bc045c16
anas Fred <anasfred1677@gmail.com>
2025-12-05 08:05:44 -0800

    commited if else branch

---
+++ if_else.py
@@ -0,0 +1,23 @@
+# age = int(input("enter your age "))
+
+# if age <= 10:
+#     #code block
+#     print("you are a young strange one age:", age)
+# elif age < 40:
+#     #code block
+#     print("the fire in your is hot strong one:", age)
+# elif age < 60:
+#     #code block
+#     print("the fire in your is hot strong one:", age)
+# else:
+#     #code block 
+#     print("you are wise beyond doubt strange one:", age)
+
+meat = input('do you eat meat? (Y/N): ')
+
+if meat == 'y':
+    #code block
+    print('here is the meat menu')
+else:
+    #code block
+    print('here is the vegetarian menu')
